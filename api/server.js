bba53ee49498e8a6a9d3bafd6d8f4bdb7379538e
kurisani-web <kurisanim2@gmail.com>
2025-07-16 11:27:24 +0200

    fix align

--- api/server.js
+++ api/server.js
@@ -6,14 +6,14 @@ require("dotenv").config();
 
 const paypalRoutes = require("./paypal");
 const notificationRoutes = require("./notifications");
-app.use("./notifications", notificationRoutes);
-
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/paypal", paypalRoutes);
+app.use("./notifications", notificationRoutes);
+
 app.get("/ping", (req, res) => {
   res.status(200).send("pong");
 });
