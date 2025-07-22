f8705bd1fc5e66c14c8fb01d6ccf8e4f2a4548c7
kurisani-web <kurisanim2@gmail.com>
2025-07-22 08:11:58 +0200

    ddf

---
+++ android/app/build/generated/autolinking/src/main/java/com/facebook/react/PackageList.java
@@ -0,0 +1,135 @@
+package com.facebook.react;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.res.Resources;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainPackageConfig;
+import com.facebook.react.shell.MainReactPackage;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+// @notifee/react-native
+import io.invertase.notifee.NotifeePackage;
+// @react-native-async-storage/async-storage
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+// @react-native-firebase/app
+import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
+// @react-native-firebase/auth
+import io.invertase.firebase.auth.ReactNativeFirebaseAuthPackage;
+// @react-native-firebase/firestore
+import io.invertase.firebase.firestore.ReactNativeFirebaseFirestorePackage;
+// @react-native-firebase/in-app-messaging
+import io.invertase.firebase.fiam.ReactNativeFirebaseFiamPackage;
+// @react-native-firebase/messaging
+import io.invertase.firebase.messaging.ReactNativeFirebaseMessagingPackage;
+// @react-native-google-signin/google-signin
+import com.reactnativegooglesignin.RNGoogleSigninPackage;
+// @sentry/react-native
+import io.sentry.react.RNSentryPackage;
+// expo
+import expo.modules.ExpoModulesPackage;
+// lottie-react-native
+import com.airbnb.android.react.lottie.LottiePackage;
+// react-native-app-auth
+import com.rnappauth.RNAppAuthPackage;
+// react-native-fs
+import com.rnfs.RNFSPackage;
+// react-native-gesture-handler
+import com.swmansion.gesturehandler.RNGestureHandlerPackage;
+// react-native-google-mobile-ads
+import io.invertase.googlemobileads.ReactNativeGoogleMobileAdsPackage;
+// react-native-immersive
+import com.rnimmersive.RNImmersivePackage;
+// react-native-localize
+import com.zoontek.rnlocalize.RNLocalizePackage;
+// react-native-navigation-bar-color
+import com.thebylito.navigationbarcolor.NavigationBarColorPackage;
+// react-native-print
+import com.christopherdro.RNPrint.RNPrintPackage;
+// react-native-safe-area-context
+import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
+// react-native-screens
+import com.swmansion.rnscreens.RNScreensPackage;
+// react-native-svg
+import com.horcrux.svg.SvgPackage;
+// react-native-vector-icons
+import com.oblador.vectoricons.VectorIconsPackage;
+// react-native-webview
+import com.reactnativecommunity.webview.RNCWebViewPackage;
+// react-native-edge-to-edge
+import com.zoontek.rnedgetoedge.EdgeToEdgePackage;
+
+public class PackageList {
+  private Application application;
+  private ReactNativeHost reactNativeHost;
+  private MainPackageConfig mConfig;
+
+  public PackageList(ReactNativeHost reactNativeHost) {
+    this(reactNativeHost, null);
+  }
+
+  public PackageList(Application application) {
+    this(application, null);
+  }
+
+  public PackageList(ReactNativeHost reactNativeHost, MainPackageConfig config) {
+    this.reactNativeHost = reactNativeHost;
+    mConfig = config;
+  }
+
+  public PackageList(Application application, MainPackageConfig config) {
+    this.reactNativeHost = null;
+    this.application = application;
+    mConfig = config;
+  }
+
+  private ReactNativeHost getReactNativeHost() {
+    return this.reactNativeHost;
+  }
+
+  private Resources getResources() {
+    return this.getApplication().getResources();
+  }
+
+  private Application getApplication() {
+    if (this.reactNativeHost == null) return this.application;
+    return this.reactNativeHost.getApplication();
+  }
+
+  private Context getApplicationContext() {
+    return this.getApplication().getApplicationContext();
+  }
+
+  public ArrayList<ReactPackage> getPackages() {
+    return new ArrayList<>(Arrays.<ReactPackage>asList(
+      new MainReactPackage(mConfig),
+      new NotifeePackage(),
+      new AsyncStoragePackage(),
+      new ReactNativeFirebaseAppPackage(),
+      new ReactNativeFirebaseAuthPackage(),
+      new ReactNativeFirebaseFirestorePackage(),
+      new ReactNativeFirebaseFiamPackage(),
+      new ReactNativeFirebaseMessagingPackage(),
+      new RNGoogleSigninPackage(),
+      new RNSentryPackage(),
+      new ExpoModulesPackage(),
+      new LottiePackage(),
+      new RNAppAuthPackage(),
+      new RNFSPackage(),
+      new RNGestureHandlerPackage(),
+      new ReactNativeGoogleMobileAdsPackage(),
+      new RNImmersivePackage(),
+      new RNLocalizePackage(),
+      new NavigationBarColorPackage(),
+      new RNPrintPackage(),
+      new SafeAreaContextPackage(),
+      new RNScreensPackage(),
+      new SvgPackage(),
+      new VectorIconsPackage(),
+      new RNCWebViewPackage(),
+      new EdgeToEdgePackage()
+    ));
+  }
+}
