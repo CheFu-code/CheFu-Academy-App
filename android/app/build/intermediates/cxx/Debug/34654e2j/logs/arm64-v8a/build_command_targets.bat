71fe76c0aaee97289aa2b8d68befd40450ff3b6a
kurisani-web <kurisanim2@gmail.com>
2025-07-22 09:24:10 +0200

    delete

--- android/app/build/intermediates/cxx/Debug/34654e2j/logs/arm64-v8a/build_command_targets.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-"C:\\Users\\Vunene Maluleke\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\Google Play Store Apps\\Learning App\\android\\app\\.cxx\\Debug\\34654e2j\\arm64-v8a" ^
-  appmodules ^
-  react_codegen_rnscreens ^
-  react_codegen_rnsvg ^
-  react_codegen_safeareacontext
