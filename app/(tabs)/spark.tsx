685ab419a39d160dcbe6d3ee6043f0d6390d7506
CheFu-code <kurisanimaluleke77@gmail.com>
2025-12-08 23:46:20 +0200

    Refactor Spark component; improve styling with dark mode support and adjust layout properties

--- app/(tabs)/spark.tsx
+++ app/(tabs)/spark.tsx
@@ -1,22 +1,24 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { RFValue } from 'react-native-responsive-fontsize';
-import { Colors } from '@/constant/Colors';
-import { MaterialIcons } from '@expo/vector-icons';
-import { scale, verticalScale } from 'react-native-size-matters';
 import SparksFeed from '@/component/Home/SparksFeed';
+import { Colors } from '@/constant/Colors';
+import useDarkMode from '@/hooks/useDarkMode';
 import { useSafeNavigation } from '@/hooks/useSafeNavigation';
+import { MaterialIcons } from '@expo/vector-icons';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { moderateScale, scale } from 'react-native-size-matters';
 
 const Spark = () => {
     const { safePush } = useSafeNavigation();
+    const { textColor, backgroundColor } = useDarkMode();
     return (
-        <SafeAreaView style={styles.container}>
+        <SafeAreaView style={[styles.container, { backgroundColor }]}>
             <View style={styles.header}>
                 <Text style={styles.title}>Spark Feed</Text>
                 <TouchableOpacity onPress={() => safePush('/addSpark')}>
                     <MaterialIcons
                         name="post-add"
-                        color={'white'}
+                        color={textColor}
                         size={scale(20)}
                     />
                 </TouchableOpacity>
@@ -32,17 +34,16 @@ export default Spark;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: Colors.BG_COLOR,
     },
     title: {
         color: Colors.PRIMARY,
-        fontSize: RFValue(22),
+        fontSize: RFValue(20),
         fontWeight: 'bold',
     },
     header: {
         justifyContent: 'space-between',
         flexDirection: 'row',
         alignItems: 'center',
-        padding: verticalScale(10),
+        paddingHorizontal: moderateScale(10),
     },
 });
