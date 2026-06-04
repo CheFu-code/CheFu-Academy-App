4d9587b81cdbcbb089afa3a50ae8af978c9f2db0
kurisani <kurisanimaluleke77@gmail.com>
2026-06-04 07:10:51 +0200

    feat: add Google services configuration and implement Android build workflow

--- constant/imageAssets.ts
+++
@@ -1,8 +0,0 @@
-export const imageAssets: Record<string, any> = {
-    "/banner1.png": require("@/assets/images/banner1.png"),
-    "/banner2.png": require("@/assets/images/banner2.png"),
-    "/banner3.png": require("@/assets/images/banner3.png"),
-    "/banner4.png": require("@/assets/images/banner4.png"),
-    "/banner5.png": require("@/assets/images/banner5.png"),
-    "/banner6.png": require("@/assets/images/banner6.png"),
-};
